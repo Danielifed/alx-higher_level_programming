03158e67f0fa6989e94b042d05692dabb96110a1
Danielifed <d.danielife@gmail.com>
2023-01-11 05:29:11 -0800

    more data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    square_of_number = []
-    for x in matrix:
-        square_of_number.append([z**2 for z in x])
-    return square_of_number
+    return [[y ** 2 for y in x] for x in matrix]
