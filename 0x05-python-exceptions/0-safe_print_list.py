b9f9e7a3ee7aa3b5e053f2780964628bbb2ca469
Danielifed <d.danielife@gmail.com>
2023-01-24 01:06:27 -0800

    python exception programs

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""
+    prints x elements of a list.
+    """
+
+
 def safe_print_list(my_list=[], x=0):
-    y = 0
-    for i in range(x):
+    nb = 0
+    for x in range(0, x):
         try:
-            print("{}".format(my_list[i]), end='')
-            y += 1
-        except IndexError:
+            print(my_list[x], end="")
+            nb += 1
+        except:
             break
-    print()
-    return 
+    print("")
+    return(nb) 
