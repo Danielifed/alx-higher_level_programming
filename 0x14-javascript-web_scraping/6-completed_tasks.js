9e720f3abb6b02a3fca0c5f20a11ee0d4296392c
Danielifed <d.danielife@gmail.com>
2023-05-23 17:49:21 +0000

    fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,29 +2,22 @@
 
 const request = require('request');
 
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+const args = process.argv.slice(2);
+const url = args[0];
+const obj = {};
 
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('', error);
-  } else if (response.statusCode !== 200) {
-    console.error('', response.statusCode);
-  } else {
-    const tasks = JSON.parse(body);
+request(url, (err, res, body) => {
+  if (err) console.log(err);
+  const tasks = JSON.parse(body);
 
-    const completedTasksByUser = tasks.reduce((acc, task) => {
-      if (task.completed) {
-        if (acc[task.userId]) {
-          acc[task.userId]++;
-        } else {
-          acc[task.userId] = 1;
-        }
-      }
-      return acc;
-    }, {});
-
-    Object.keys(completedTasksByUser).forEach((userId) => {
-      console.log(`User ${userId}: ${completedTasksByUser[userId]} completed task(s)`);
-    });
+  for (const task of tasks) {
+    const id = task.userId.toString();
+    if (!obj[id] && task.completed) {
+      obj[id] = 1;
+    } else if (task.completed) {
+      obj[id]++;
+    }
   }
+
+  console.log(obj);
 });
