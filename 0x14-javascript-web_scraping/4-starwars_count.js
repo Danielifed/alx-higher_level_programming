da2c1307dbe64c2611c19c01b72f257ad8a82b69
Danielifed <d.danielife@gmail.com>
2023-05-23 16:33:11 +0000

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,17 @@
 #!/usr/bin/node
-
 const request = require('request');
+const args = process.argv.slice(2);
+const url = args[0];
 
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
-const characterId = '18';
-
-request(apiUrl, (err, res, body) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
+request(url, (err, res, body) => {
+  if (err) console.log(err);
   const films = JSON.parse(body).results;
   let count = 0;
 
   for (const film of films) {
-    if (film.characters.includes(characterId)) {
-      count++;
-    }
+    film.characters.forEach(character => {
+      if (character.includes('18')) count++;
+    });
   }
-
   console.log(count);
 });
